5f24eeef026377c986de8df2911e4414d23ee4d9
Puspak29 <samantapuspak@gmail.com>
2024-06-26 13:17:38 +0530

    optimized the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import Buttons from './components/Buttons.jsx'
+
+const buttonEllements =[
+  {color: 'Red', text: 'text-white'},
+  {color: 'Green', text: 'text-white'},
+  {color: 'Blue', text: 'text-white'},
+  {color: 'Lime', text: 'text-white'},
+  {color: 'Olive', text: 'text-white'},
+  {color: 'Yellow', text: 'text-black'},
+  {color: 'Teal', text: 'text-white'},
+  {color: 'Gray', text: 'text-white'},
+  {color: 'Coral', text: 'text-white'},
+  {color: 'Pink', text: 'text-black'},
+  {color: 'Purple', text: 'text-white'},
+  {color: 'Lavender', text: 'text-black'},
+  {color: 'Turquoise', text: 'text-black'},
+  {color: 'Brown', text: 'text-white'},
+  {color: 'Violet', text: 'text-black'},
+  {color: 'White', text: 'text-black'},
+  {color: 'Black', text: 'text-white'}
+]
+
 function App() {
   const [bgColor, setBgColor] = useState('white');
 
@@ -10,23 +31,15 @@ function App() {
     >
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-          <Buttons color="Red" text="text-white" customClick={()=>setBgColor("Red")} />
-          <Buttons color="Green" text="text-white" customClick={()=>setBgColor("Green")} />
-          <Buttons color="Blue" text="text-white" customClick={()=>setBgColor("Blue")} />
-          <Buttons color="Lime" text="text-white" customClick={()=>setBgColor("Lime")} />
-          <Buttons color="Olive" text="text-white" customClick={()=>setBgColor("Olive")} />
-          <Buttons color="Yellow" text="text-black" customClick={()=>setBgColor("Yellow")} />
-          <Buttons color="Teal" text="text-white" customClick={()=>setBgColor("Teal")} />
-          <Buttons color="Gray" text="text-white" customClick={()=>setBgColor("Gray")} />
-          <Buttons color="Coral" text="text-white" customClick={()=>setBgColor("Coral")} />
-          <Buttons color="Pink" text="text-black" customClick={()=>setBgColor("Pink")} />
-          <Buttons color="Purple" text="text-white" customClick={()=>setBgColor("Purple")} />
-          <Buttons color="Lavender" text="text-black" customClick={()=>setBgColor("Lavender")} />
-          <Buttons color="Turquoise" text="text-black" customClick={()=>setBgColor("Turquoise")} />
-          <Buttons color="Brown" text="text-white" customClick={()=>setBgColor("Brown")} />
-          <Buttons color="Violet" text="text-black" customClick={()=>setBgColor("Violet")} />
-          <Buttons color="White" text="text-black" customClick={()=>setBgColor("White")} />
-          <Buttons color="Black" text="text-white"  customClick={()=>setBgColor("Black")} />
+          {
+            buttonEllements.map((item)=>(
+              <Buttons
+              color={item.color}
+              text={item.text}
+              customClick={()=>setBgColor(item.color)} 
+              />
+            ))
+          }
         </div>
       </div>
     </div>
